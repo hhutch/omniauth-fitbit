9782224d4a906982a7664cdebce5c6bc01d2c6d4
Hunter Hutchinson <hunter.hutchinson@gmail.com>
2014-12-04 11:40:25 -0500

    add display option

--- lib/omniauth/strategies/fitbit.rb
+++ lib/omniauth/strategies/fitbit.rb
@@ -14,6 +14,8 @@ module OmniAuth
           :authorize_path     => '/oauth/authorize'
       }
 
+      option :authorize_options, [:scope, :display, :auth_type]
+      
       uid do
         access_token.params['encoded_user_id']
       end
@@ -49,6 +51,23 @@ module OmniAuth
           @raw_info ||= MultiJson.load(access_token.get('https://api.fitbit.com/1/user/-/profile.json').body)
         end
       end
+
+      # You can pass +display+, +scope+, or +auth_type+ params to the auth request, if you need to set them dynamically.
+      # You can also set these options in the OmniAuth config :authorize_params option.
+      #
+      # For example: /auth/facebook?display=popup
+      def authorize_params
+        super.tap do |params|
+          %w[display scope auth_type].each do |v|
+            if request.params[v]
+              params[v.to_sym] = request.params[v]
+            end
+          end
+
+          params[:scope] ||= DEFAULT_SCOPE
+        end
+      end
+      
     end
   end
 end
